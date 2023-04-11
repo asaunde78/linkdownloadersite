603c9ba995e591b7a1a423835eb8652b882da8dd
asaunde78 <saundersasher78@gmail.com>
2023-04-10 23:16:36 -0400

    added the file downloading functionality

--- linkdownloader.py
+++ linkdownloader.py
@@ -1,12 +1,14 @@
 from flask import Flask, current_app
 # from PIL import Image
-
+import requests,os,shutil
 
 class downloader():
-    def __init__(self):
+    def __init__(self, folder="images"):
         self.app = Flask(__name__)
         # img = Image.open(f'small.png')
         # @app.route('/')
+        self.folder = folder
+        
         with open("links.txt", "w") as r:
             r.write("")
         self.setup_routes()
@@ -17,8 +19,16 @@ class downloader():
         def index(path):
             # print(path)
             url = path#.split("?")[0]
+            filename = url.split("/")[-1]
+            if("?" in filename):
+                filename = filename.split("?")[0]
             with open("links.txt", "a") as r:
-                r.write(url+"\n")
+                r.write(filename+"\n")
+            # ftype = filename.split(".")[-1]
+            with requests.get(url, allow_redirects=True) as response, open(self.folder+"/" +filename, 'wb') as f:
+                #print(response.text)
+                data = response.content
+                f.write(data)
             return current_app.send_static_file("small.png")
             # return render_template("index.html",name=path)
     def run(self):
